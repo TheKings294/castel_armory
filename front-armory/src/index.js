9759108c9319c76b04ac7a654f3c8591a262a0c7
Johnny Sassiat <johnny.sass2001@gmail.com>
2026-02-02 11:54:42 +0100

    page-home-sans-css

--- front-armory/src/index.js
+++ front-armory/src/index.js
@@ -1,15 +1,19 @@
-// index.js
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Armurie from "./template/templates/armurie"; // assure-toi d'importer ce composant
+import CourDuChateau from "./template/templates/cour-du-chateau";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const isConnected = true;
+
 root.render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<CourDuChateau />} />
+      <Route
+        path="/Armurie"
+        element={isConnected ? <Armurie /> : <Navigate to="/" replace />}
+      />
+    </Routes>
   </BrowserRouter>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
