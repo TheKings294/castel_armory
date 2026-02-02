99e33b61709dba4b34dcec8b0bc53fb271a2d959
TheKings294 <98644094+TheKings294@users.noreply.github.com>
2026-02-02 10:33:46 +0100

    feat: add api basal

---
+++ back-end/src/test/java/com/armory/api/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.armory.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
