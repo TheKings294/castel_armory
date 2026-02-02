3765ebf6d01a8d28b7d332df34e1eb8dc0632218
Mockingjay <mockingjay@MacBook-Pro-de-Mockingjay.local>
2026-02-02 14:44:34 +0100

    background taverne & carte de la taverne

--- front-armory/src/App.js
+++ front-armory/src/App.js
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import LoginModal from "./template/login.jsx";
+import Tavern from "./template/templates/taverne.jsx";
 
 function App() {
     const [loginOpen, setLoginOpen] = useState(false);
 
     return (
         <div>
+            <Tavern />
+
             <button onClick={() => setLoginOpen(true)}>
                 Se connecter
             </button>
